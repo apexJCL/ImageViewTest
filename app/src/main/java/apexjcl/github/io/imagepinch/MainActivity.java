19ca7f1144fe7eed84839d6191181835daf1be67
José Carlos López <jcl.isc@gmail.com>
2016-10-04 21:40:32 -0500

    init

---
+++ app/src/main/java/apexjcl/github/io/imagepinch/MainActivity.java
@@ -0,0 +1,19 @@
+package apexjcl.github.io.imagepinch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        PhotoViewAttacher attacher = new PhotoViewAttacher(imageView);
+    }
+}
